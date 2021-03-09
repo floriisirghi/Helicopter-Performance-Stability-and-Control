529c553a0cb6e9cc5a787e37a8b4b055b1a8364c
MartijnKanger <MartijnKanger@users.noreply.github.com>
2021-03-09 11:24:59 +0100

    update

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Parameters import *
+
+"""
+Ideal Power
+"""
+T = W
+v_i = np.sqrt(T/(2*rho_SL*np.pi*R_main**2))
+P_i = T*v_i
+
+"""
+Hover Power ACT
+"""
+P_hov_ACT = (W/M)*np.sqrt(W/(2*np.pi*rho_SL*R_main**2))
+
+"""
+Hover Power BEM
+"""
+P_hov_BEM = T*v_i + (CDp/8)*rho_SL*sigma*(Omega*R_main)**3*np.pi*R_main**2
+
+print(P_hov_BEM/P_hov_ACT)
+# 30% Difference is quite a lot, we might have to tweak the assumed parameters. Probably CDp is assumed too high.
--- Parameters.py
+++ Parameters.py
@@ -4,17 +4,18 @@ Eurocopter AS365 N3 Dauphin
 """
 
 import math as m
+import numpy as np
+#------------------- Global parameters  -----------------------
+#------------------- Physical constants -----------------------
+g = 9.80665         # [kgm/s^2] Gravity
+rho_SL = 1.225       #[kg/m^3] density at sea level
 
-#------------------- Global parameters -----------------------
-# These are constants that are defined here globally and may be imported and used when needed
-
-g = 9.80665          #[kg/m*s^2] gravitational acceleration
+#------------------- Helicopter parameters -----------------------
 mass = 4300          #[kg] "maximum all up weight"
 W = 4300*g           #[N] the actual weight of the helicopter
 D_main = 11.94       #[m] main rotor diameter
 R_main = D_main/2    #[m] main rotor radius
 D_tail = 1.1         #[m] tail rotor diameter
-rho_SL = 1.225       #[kg/m^3] density at sea level
 P_TO = 635           #[kW] take-off power
 P_cont = 597         #[kW] continuous power
 Disc_L = W/(m.pi * (R_main**2)) #[N/m^2] disc loading
@@ -22,3 +23,9 @@ v_i_hov = 12.3983    #[m/s] rotor induced velocity in hover
 sum_CD_S = 1         #[m^2] "flat plate area"
 tip_speed_main = 218 #[m/s] this value is taken from the brightspace database, so for the older version of the helicopter
 tip_speed_tail = 227 #[m/s] this value is taken from the brightspace database, so for the older version of the helicopter
+N_Blades = 4         #[/] Number of blades.
+c = 0.3              #[m] Average chord.                    Assumption
+M = 0.7              #[/] Figure of merit.                  Assumption
+CDp = 0.04           #[/] Average drag coefficient.         Assumption
+Omega = tip_speed_main/(R_main)     #[/s] Rotational rate.      Could be angular, then correct with 2pi, have to investigate
+sigma = N_Blades*c/(np.pi*R_main)   # Rotor solidity.       Uses assumptions as input
