d1e9ffdc7d60e36deba8e88417f4d184760023fb
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2021-03-11 15:18:33 +0100

    Added the tail rotor power calculations

--- Assignment2Question2.py
+++ Assignment2Question2.py
@@ -1,14 +1,19 @@
 from Parameters import *
-import math as m
+import numpy as np
+from Assignment1 import *
 import matplotlib.pyplot as plt
 
 def rotor_power_forward_flight(V):
-# Calculate the helicopter rotor power in forward flight. For this determine the parasite drag power,
-# induced power and total profile drag power of the rotor.
+
+    # 1. Calculate the helicopter rotor power in forward flight. For this determine the parasite drag power,
+    #    induced power and total profile drag power of the rotor.
+
+    #-----------------MAIN ROTOR-----------------------------
 
     #---------------- Parasite drag power -------------------
     D_par = sum_CD_S*1/2*rho_SL*(V**2)
     P_par = D_par * V
+    print(V)
 
     #---------------- Profile drag power --------------------
     mu = V/tip_speed_main #tip speed ratio
@@ -17,15 +22,41 @@ def rotor_power_forward_flight(V):
 
     #--------------- Induced power ---------------------------
     V_bar = V/v_i_hov
-    v_i_bar = np.sqrt(-((V_bar**2)/2)+np.sqrt((V_bar**4)/4 + 1))
-    P_i = k*W*v_i_bar*np.sqrt(W/(2*rho_SL*m.pi*R_main**2)) #this is from equation 35a. Should we just use k*T*v_i as in the slides?
+    #v_i_bar = 1/V_bar #works only for high speeds
+    v_i_bar = np.sqrt(-((V_bar ** 2) / 2) + np.sqrt((V_bar ** 4) / 4 + 1))
 
-    #----------- Total rotor power in forward flight ---------
-    P_tot = P_par + P_p_and_P_d + P_i
+    P_i = k*W*v_i_bar*np.sqrt(W/(2*rho_SL*np.pi*R_main**2)) #from equation 35a
+
+    #----------- Total main rotor power in forward flight ---------
+    P_tot_main = P_par + P_p_and_P_d + P_i
+
+
+
+    # 2. Calculate the tail rotor power using BEM theory
+    # ----------------- TAIL ROTOR-----------------------------
+
+    # ---------------- Profile drag power --------------------
+    mu_tail = V / tip_speed_tail  # tip speed ratio
+
+    P_p_and_P_d_tail = (sigma_tail * CDp) / 8 * rho_SL * (tip_speed_tail ** 3) * m.pi * (R_tail ** 2) * (1 + 4.65 * (mu_tail ** 2))
+
+    # --------------- Induced power ---------------------------
+    V_bar = V / v_i_hov
+
+    #v_i_bar = 1/V_bar #works only for high speeds
+    v_i_bar = np.sqrt(-((V_bar ** 2) / 2) + np.sqrt((V_bar ** 4) / 4 + 1))
+
+    T_tail = P_hov_BEM/((tip_speed_tail/R_tail)*l_tr)
+
+    P_i_tail = 1.1*k_tail*T_tail*v_i_bar*v_i_hov  # from equation 35a
+
+    # ----------- Total main rotor power in forward flight ---------
+    P_tot_tail = P_p_and_P_d_tail + P_i_tail
+
+    P_tot = P_tot_main + P_tot_tail
 
     return P_tot
 
-print(rotor_power_forward_flight(150))
 
 V = np.linspace(0,90,101)
 Power = 1.1*rotor_power_forward_flight(V)/1000
--- Parameters.py
+++ Parameters.py
@@ -16,18 +16,27 @@ W = 4300*g           #[N] the actual weight of the helicopter
 D_main = 11.94       #[m] main rotor diameter
 R_main = D_main/2    #[m] main rotor radius
 D_tail = 1.1         #[m] tail rotor diameter
+R_tail = D_tail/2    #[m] tail rotor radius
 P_TO = 635           #[kW] take-off power
 P_cont = 597         #[kW] continuous power
 Disc_L = W/(m.pi * (R_main**2)) #[N/m^2] disc loading
 v_i_hov = 12.3983    #[m/s] rotor induced velocity in hover
 sum_CD_S = 1         #[m^2] "flat plate area"
-tip_speed_main = 218 #[m/s] this value is taken from the brightspace database, so for the older version of the helicopter
+tip_speed_main = 218 #[m/s] omega*R this value is taken from the brightspace database, so for the older version of the helicopter
 tip_speed_tail = 227 #[m/s] this value is taken from the brightspace database, so for the older version of the helicopter
-N_Blades = 4         #[/] Number of blades.
-c = 0.385             #[m] Average chord.                    Assumption
+N_Blades = 4         #[/] Number of blades main rotor.
+N_blades_tail = 10   #[/] Number of blades fenestron.
+c = 0.3
+#c = 0.385            #[m] Average chord main rotor.                    Assumption -> brightspace database
+c_tail = 0.094       #[m] Average chord fenestron.
 M = 0.7              #[/] Figure of merit.                  Assumption
-CDp = 0.015           #[/] Average drag coefficient.         Assumption
+#CDp = 0.015           #[/] Average drag coefficient.         Assumption
+CDp = 0.04
 Omega = tip_speed_main/(R_main)     #[/s] Rotational rate.      Could be angular, then correct with 2pi, have to investigate
 sigma = N_Blades*c/(np.pi*R_main)   # Rotor solidity.       Uses assumptions as input
-k = 1.15             # induced drag power factor; this parameter has a value between 1.1 and 1.2 as stated in the reader, so we can assume it to be 1.15
-
+sigma_tail = N_blades_tail*c_tail/(np.pi*R_tail) # Tail rotor solidty. Found to be 0.49 in database but for slightly older model
+k = 1.15             # induced drag power factor; this parameter has a value between 1.1 and 1.2 as stated in the reader,
+                     # so we can assume it to be 1.15
+k_tail = 1.4         # induced drag power factor for the tail; this parameter has a value between 1.3 and 1.5
+                     # as stated in the assignment description, so we can assume it to be 1.4
+l_tr = 6.98          # [m] tail rotor length. Measured on the sketch
