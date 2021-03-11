9ab03beaf3ad35480f1bf04f5f900c82872f640e
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2021-03-11 10:36:53 +0100

    Made some updates to the power calculations

--- Assignment 2 Question 2.py
+++ Assignment 2 Question 2.py
@@ -1,9 +1,10 @@
 from Parameters import *
 import math as m
 
+# 1. Calculate the helicopter rotor power in forward flight. For this determine the parasite drag power,
+#    induced power and total profile drag power of the rotor.
+
 def rotor_power_forward_flight(V):
-# Calculate the helicopter rotor power in forward flight. For this determine the parasite drag power,
-# induced power and total profile drag power of the rotor.
 
     #---------------- Parasite drag power -------------------
     D_par = sum_CD_S*1/2*rho_SL*(V**2)
@@ -12,11 +13,12 @@ def rotor_power_forward_flight(V):
     #---------------- Profile drag power --------------------
     mu = V/tip_speed_main #tip speed ratio
 
-    P_p_and_P_d = (sigma*CDp)/8*rho_SL*(tip_speed_main**3)*m.pi*(R_main**2)*(1+4.65*(mu**2)) 
+    P_p_and_P_d = (sigma*CDp)/8*rho_SL*(tip_speed_main**3)*m.pi*(R_main**2)*(1+4.65*(mu**2))
 
     #--------------- Induced power ---------------------------
     V_bar = V/v_i_hov
-    v_i_bar = m.sqrt(-((V_bar**2)/2)+m.sqrt((V_bar**4)/4 + 1))
+    #v_i_bar = m.sqrt(-((V_bar**2)/2)+m.sqrt((V_bar**4)/4 + 1))
+    v_i_bar = v_i_hov/V
     P_i = k*W*v_i_bar*m.sqrt(W/(2*rho_SL*m.pi*R_main**2)) #this is from equation 35a. Should we just use k*T*v_i as in the slides?
 
     #----------- Total rotor power in forward flight ---------
@@ -24,4 +26,7 @@ def rotor_power_forward_flight(V):
 
     return P_tot
 
-print(rotor_power_forward_flight(150))
+print(rotor_power_forward_flight(100))
+
+# 2. Calculate the helicopter rotor power in forward flight. For this determine the parasite drag power,
+#    induced power and total profile drag power of the rotor. 
