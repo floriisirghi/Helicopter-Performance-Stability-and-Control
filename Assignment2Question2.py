7b6029766efdf1f9ee25c810c717bc5de53f6bcd
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2021-03-30 11:20:55 +0200

    Updated v_i_hov_tail calculation

--- Assignment2Question2.py
+++ Assignment2Question2.py
@@ -13,18 +13,25 @@ def rotor_power_forward_flight(V):
     #---------------- Parasite drag power -------------------
     D_par = sum_CD_S*1/2*rho_SL*(V**2)
     P_par = D_par * V
-    print(V)
 
     #---------------- Profile drag power --------------------
     mu = V/tip_speed_main #tip speed ratio
 
-    P_p_and_P_d = (sigma*CDp)/8*rho_SL*(tip_speed_main)**3*m.pi*(R_main**2)*(1+4.65*(mu**2)) 
+    P_p_and_P_d = (sigma*CDp)/8*rho_SL*(tip_speed_main**3)*m.pi*(R_main**2)*(1+4.65*(mu**2))
 
     #--------------- Induced power ---------------------------
     V_bar = V/v_i_hov
-    #v_i_bar = 1/V_bar #works only for high speeds
+
+
+    #v_i_bar_low = np.sqrt(-((V_bar[0:60] ** 2) / 2) + np.sqrt((V_bar[0:60] ** 4) / 4 + 1))
+    #v_i_bar_high = T/(2*rho_SL*np.pi*R_main**2*V[60:])/v_i_hov
+
+
+    #v_i_bar = np.concatenate([v_i_bar_low,v_i_bar_high])
+
     v_i_bar = np.sqrt(-((V_bar ** 2) / 2) + np.sqrt((V_bar ** 4) / 4 + 1))
 
+    print(v_i_bar)
     P_i = k*W*v_i_bar*np.sqrt(W/(2*rho_SL*np.pi*R_main**2)) #from equation 35a
 
     #----------- Total main rotor power in forward flight ---------
@@ -41,14 +48,16 @@ def rotor_power_forward_flight(V):
     P_p_and_P_d_tail = (sigma_tail * CDp) / 8 * rho_SL * (tip_speed_tail ** 3) * m.pi * (R_tail ** 2) * (1 + 4.65 * (mu_tail ** 2))
 
     # --------------- Induced power ---------------------------
-    V_bar = V / v_i_hov
+    T_tail = P_hov_BEM / ((tip_speed_tail / R_tail) * l_tr)
+
+    v_i_hov_tail = np.sqrt(T_tail/(2*np.pi*rho_SL*R_tail**2))
+    V_bar_tail = V / v_i_hov_tail
 
     #v_i_bar = 1/V_bar #works only for high speeds
-    v_i_bar = np.sqrt(-((V_bar ** 2) / 2) + np.sqrt((V_bar ** 4) / 4 + 1))
+    v_i_bar_tail = np.sqrt(-((V_bar_tail ** 2) / 2) + np.sqrt((V_bar_tail ** 4) / 4 + 1))
 
-    T_tail = P_hov_BEM/((tip_speed_tail/R_tail)*l_tr)
 
-    P_i_tail = 1.1*k_tail*T_tail*v_i_bar*v_i_hov  # from equation 35a
+    P_i_tail = 1.1*k_tail*T_tail*v_i_bar_tail*v_i_hov_tail # from equation 35a
 
     # ----------- Total main rotor power in forward flight ---------
     P_tot_tail = P_p_and_P_d_tail + P_i_tail
@@ -59,6 +68,18 @@ def rotor_power_forward_flight(V):
 
 
 V = np.linspace(0,100,101)
+Power = 1.1*rotor_power_forward_flight(V)/1000
+plt.plot(V,Power)
+plt.xlabel("Velocity [m/s]")
+plt.ylabel("Power [kW]")
+plt.plot(V,1270*np.ones(len(V)),label='Max TO')
+plt.plot(V,1194*np.ones(len(V)),label='Max CO')
+plt.legend()
+plt.title(" Power velocity curve with Max Take-Off and Max Continuous power.")
+plt.show()
+
+"""
+V = np.linspace(0,100,101)
 Power = rotor_power_forward_flight(V)/1000
 plt.plot(V,Power)
 plt.plot(V,0.98*1270*np.ones(len(V)),label='Max TO')
@@ -66,3 +87,4 @@ plt.plot(V,0.98*1194*np.ones(len(V)),label='Max CO')
 plt.legend()
 plt.title(" Power velocity curve with Max Take-Off and Max Continuous power.")
 plt.show()
+"""
