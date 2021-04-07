3fdba2669876f4d15b9fc159849ebde28078e0bd
Florina Sirghi <f.sirghi-1@student.tudelft.nl>
2021-04-07 11:55:59 +0200

    The latest version of the code of Assignment 2

--- Assignment2Question2.py
+++ Assignment2Question2.py
@@ -23,15 +23,15 @@ def rotor_power_forward_flight(V):
     V_bar = V/v_i_hov
 
 
-    #v_i_bar_low = np.sqrt(-((V_bar[0:60] ** 2) / 2) + np.sqrt((V_bar[0:60] ** 4) / 4 + 1))
-    #v_i_bar_high = T/(2*rho_SL*np.pi*R_main**2*V[60:])/v_i_hov
+    #v_i_bar_low = np.sqrt(-((V_bar[0:20] ** 2) / 2) + np.sqrt((V_bar[0:20] ** 4) / 4 + 1))
+    #v_i_bar_med = (np.sqrt(-((V_bar[20:40] ** 2) / 2) + np.sqrt((V_bar[20:40] ** 4) / 4 + 1)) + T/(2*rho_SL*np.pi*R_main**2*V[20:40])/v_i_hov)/2
+    #v_i_bar_high = T/(2*rho_SL*np.pi*R_main**2*V[40:])/v_i_hov
 
 
-    #v_i_bar = np.concatenate([v_i_bar_low,v_i_bar_high])
+    #v_i_bar = np.concatenate([v_i_bar_low,v_i_bar_med, v_i_bar_high])
 
     v_i_bar = np.sqrt(-((V_bar ** 2) / 2) + np.sqrt((V_bar ** 4) / 4 + 1))
 
-    print(v_i_bar)
     P_i = k*W*v_i_bar*np.sqrt(W/(2*rho_SL*np.pi*R_main**2)) #from equation 35a
 
     #----------- Total main rotor power in forward flight ---------
@@ -64,25 +64,55 @@ def rotor_power_forward_flight(V):
 
     P_tot = P_tot_main + P_tot_tail
 
-    return P_tot
+    return P_tot, P_tot_tail, P_par, P_p_and_P_d, P_i, v_i_bar
 
 
 V = np.linspace(0,100,101)
-Power = 1.1*rotor_power_forward_flight(V)/1000
-plt.plot(V,Power)
+P_tot, P_tot_tail, P_par, P_p_and_P_d, P_i, v_i_bar = rotor_power_forward_flight(V)
+Power = P_tot/1000  #why do we have this 1.1 here?
+#plt.plot(V,P_tot_tail/1000, label="Tail rotor total power")
+plt.plot(V,Power, label="Total power")
+#plt.plot(V, v_i_bar, label="Induced velocity")
+#plt.plot(V,P_par/1000, label="Parasite drag power")
+#plt.plot(V,P_p_and_P_d/1000, label="Total profile drag power")
+#plt.plot(V, P_i/1000, label="Induced power")
 plt.xlabel("Velocity [m/s]")
 plt.ylabel("Power [kW]")
-plt.plot(V,1270*np.ones(len(V)),label='Max TO')
-plt.plot(V,1194*np.ones(len(V)),label='Max CO')
+plt.plot(V,1270*0.95*np.ones(len(V)),label='Max TO')
+plt.plot(V,1194*0.95*np.ones(len(V)),label='Max CO')
 plt.legend()
 plt.title(" Power velocity curve with Max Take-Off and Max Continuous power.")
 plt.show()
 
+#print(P_par[60]+ P_i[60] + P_p_and_P_d[60])
+#print(P_par[60])
+#print(P_i[60])
+#print(P_p_and_P_d[60])
+
 """
+eta_m = 0.95        #Mechanical efficiency
+P_max_TO = 1270*eta_m
+P_max_CO = 1194*eta_m
+
 V = np.linspace(0,100,101)
 Power = rotor_power_forward_flight(V)/1000
 plt.plot(V,Power)
-plt.plot(V,0.98*1270*np.ones(len(V)),label='Max TO')
+plt.xlabel("Velocity [m/s]")
+plt.ylabel("Power [kW]")
+plt.plot(V,P_max_TO*np.ones(len(V)),label='Max TO')
+plt.plot(V,P_max_CO*np.ones(len(V)),label='Max CO')
+plt.legend()
+plt.title(" Power velocity curve with Max Take-Off and Max Continuous power.")
+plt.show()
+
+
+V = np.linspace(0,100,101)
+Power = rotor_power_forward_flight(V)/1000
+plt.plot(V,Power)
+plt.plot(V,0.98*1270*np.ones(len(V)),label='Max TO') 
+
+#why do we have this 0.98 here?
+
 plt.plot(V,0.98*1194*np.ones(len(V)),label='Max CO')
 plt.legend()
 plt.title(" Power velocity curve with Max Take-Off and Max Continuous power.")
--- Parameters.py
+++ Parameters.py
@@ -8,7 +8,7 @@ import numpy as np
 #------------------- Global parameters  -----------------------
 #------------------- Physical constants -----------------------
 g = 9.80665         # [kgm/s^2] Gravity
-rho_SL = 1.1       #[kg/m^3] density at sea level
+rho_SL = 1.225       #[kg/m^3] density at usual operation altitude
 
 #------------------- Helicopter parameters -----------------------
 mass = 4300          #[kg] "maximum all up weight"
@@ -19,8 +19,8 @@ D_tail = 1.1         #[m] tail rotor diameter
 R_tail = D_tail/2    #[m] tail rotor radius
 P_TO = 635           #[kW] take-off power
 P_cont = 597         #[kW] continuous power
-Disc_L = W/(m.pi * (R_main**2)) #[N/m^2] disc loading
-v_i_hov = 12.3983    #[m/s] rotor induced velocity in hover
+Disc_L = W/(np.pi * (R_main**2)) #[N/m^2] disc loading
+v_i_hov = np.sqrt(W/(2*rho_SL*np.pi*R_main**2))    #[m/s] rotor induced velocity in hover
 sum_CD_S = 1         #[m^2] "flat plate area"
 tip_speed_main = 218 #[m/s] omega*R this value is taken from the brightspace database, so for the older version of the helicopter
 tip_speed_tail = 227 #[m/s] this value is taken from the brightspace database, so for the older version of the helicopter
@@ -31,15 +31,13 @@ c = 0.385            #[m] Average chord main rotor.                    Assumptio
 c_tail = 0.094       #[m] Average chord fenestron.
 M = 0.7              #[/] Figure of merit.                  Assumption
 Omega = tip_speed_main/(R_main)     #[/s] Rotational rate.      Could be angular, then correct with 2pi, have to investigate
-sigma = N_Blades*c/(np.pi*R_main)   # Rotor solidity.       Uses assumptions as input
+sigma = N_Blades*c/(np.pi*R_main) # Rotor solidity.       Uses assumptions as input
 sigma_tail = N_blades_tail*c_tail/(np.pi*R_tail) # Tail rotor solidty. Found to be 0.49 in database but for slightly older model
 C_L_mean = 6.6*(W/(rho_SL*m.pi*(R_main**2)*(tip_speed_main**2)))/sigma #Medium lift coefficient
-print(C_L_mean)
 a_SL = 343           #[m/s] Speed of sound at sea level
 V_cruise = 74.7      #[m/s] Recommended cruise speed
 M_t = (tip_speed_main + V_cruise) /a_SL #Tip Mach number
-print(M_t)
-CDp = 0.034          #[/] Mean profile drag coefficient.         Determined graphically
+CDp = 0.031          #[/] Mean profile drag coefficient.         Determined graphically
 k = 1.15             # induced drag power factor; this parameter has a value between 1.1 and 1.2 as stated in the reader,
                      # so we can assume it to be 1.15
 k_tail = 1.4         # induced drag power factor for the tail; this parameter has a value between 1.3 and 1.5
