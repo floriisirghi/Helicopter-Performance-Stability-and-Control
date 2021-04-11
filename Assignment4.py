1ae61c78d3a0ec3d54c64c848c03967b72c42bea
MartijnKanger <MartijnKanger@users.noreply.github.com>
2021-04-11 16:22:04 +0200

    push

---
+++ Assignment4.py
@@ -0,0 +1,136 @@
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 22})
+
+"""
+Constants
+"""
+Vtip = 218
+R = 11.94/2
+Omega = Vtip/R
+gamma = 6
+cla = 5.7
+sigma = 0.075
+rho = 1.225
+CD = 1
+S = 1.5
+g = 9.81
+m = 4300
+Iy = 10000
+h = 1
+
+"""
+While loop definition
+"""
+Tstart = 0
+Tstop = 120
+N_timesteps = 1200
+dt = Tstop/N_timesteps
+
+t = 0
+
+"""
+Control inputs
+"""
+theta_0 = 6*np.pi/180
+theta_c = 0
+
+"""
+State variables
+"""
+u = 46.3/2
+w = 0
+q = 0
+theta_f = 0
+
+"""
+Initialize result arrays
+"""
+lambda_i = np.sqrt(m*g/(2*rho*(np.pi*R**2)))/Vtip; # Initial guess
+ulst = []
+wlst = []
+qlst = []
+theta_flst = []
+tlst = []
+wdot = 0
+#thetaclst = []
+while t < Tstop:
+
+    #theta_c = -1.2*theta_f
+    #theta_0 = -0.2*wdot - 0.2*w
+
+    V = np.sqrt(u**2 + w**2)
+
+    if u == 0:
+        if w > 0:
+            alpha_c = theta_c + np.pi/2
+        else:
+            alpha_c = theta_c - np.pi/2
+    else:
+        alpha_c = theta_c - np.arctan2(w,u)
+    
+    mu = ((V)/(Omega*R))*np.cos(alpha_c)
+    lambda_c = (V*np.sin(alpha_c))/(Omega*R)
+
+    a1 = ((8*mu*theta_0)/(3) - 2*mu*(lambda_c + lambda_i) - (16*q)/(gamma*Omega))/(1 - 0.5*mu**2)
+
+    def CTbem(lambda_i):
+        return(0.25*cla*sigma*((2*theta_0*(1 + (3*mu**2)/(2)))/(3) - (lambda_c + lambda_i)))
+    def CTGlau(lambda_i):
+        return(2*lambda_i*np.sqrt(((V*np.cos(alpha_c-a1))/(Omega*R))**2 + ((V*np.sin(alpha_c-a1))/(Omega*R) + lambda_i )**2))
+    def F(lambda_i):
+        return CTbem(lambda_i) - CTGlau(lambda_i)
+
+    lambda_i = fsolve(F,[0])[0]
+    print(lambda_i)
+    a1 = ((8*mu*theta_0)/(3) - 2*mu*(lambda_c + lambda_i) - (16*q)/(gamma*Omega))/(1 - 0.5*mu**2)
+    CT = CTGlau(lambda_i)
+
+    T = CT*rho*(Omega*R)**2*np.pi*R**2
+    D = CD*S*0.5*rho*V**2
+    
+    udot = -g*np.sin(theta_f) - (D*u)/(m*V) + (T*np.sin(theta_c-a1))/(m) - q*w
+    wdot = g*np.cos(theta_f) - (D*w)/(m*V) - (T*np.cos(theta_c-a1))/(m) + q*u
+    qdot = (-T*h*np.sin(theta_c-a1))/(Iy)
+    theta_fdot = q
+
+    u += udot*dt
+    w += wdot*dt
+    q += qdot*dt
+    theta_f += theta_fdot*dt
+    t += dt
+
+    ulst += [u]
+    wlst += [w]
+    qlst += [q]
+    theta_flst += [theta_f]
+    tlst += [t]
+    #thetaclst += [theta_c]
+
+#plt.plot(tlst,thetaclst)
+#plt.ylabel('theta_c(m/s)',rotation=0)
+#plt.xlabel('t(s)')
+#plt.legend
+#plt.show()
+plt.plot(tlst,ulst)
+plt.ylabel('u(m/s)',rotation=0)
+plt.xlabel('t(s)')
+plt.legend
+plt.show()
+plt.plot(tlst,wlst)
+plt.ylabel('w(m/s)',rotation=0)
+plt.xlabel('t(s)')
+plt.legend
+plt.show()
+plt.plot(tlst,qlst)
+plt.ylabel('q(?)',rotation=0)
+plt.xlabel('t(s)')
+plt.legend
+plt.show()
+plt.plot(tlst,theta_flst)
+plt.ylabel(r'$\theta_f$(deg/rad?)',rotation=0)
+plt.xlabel('t(s)')
+plt.legend
+plt.show()
+
